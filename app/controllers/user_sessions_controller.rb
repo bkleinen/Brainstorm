0a1aacef56de8a03fd8a73ff9ca444ab3bf7fd2b
B Kleinen <kleinen@infrastructure.de>
2011-11-22 02:16:06 +0100

    authlogic basic working

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,28 +1,4 @@
 class UserSessionsController < ApplicationController
-  # GET /user_sessions
-  # GET /user_sessions.json
-  def index
-    @user_sessions = UserSession.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @user_sessions }
-    end
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.json
-  def show
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user_session }
-    end
-  end
-
-  # GET /user_sessions/new
-  # GET /user_sessions/new.json
   def new
     @user_session = UserSession.new
 
@@ -32,19 +8,12 @@ class UserSessionsController < ApplicationController
     end
   end
 
-  # GET /user_sessions/1/edit
-  def edit
-    @user_session = UserSession.find(params[:id])
-  end
-
-  # POST /user_sessions
-  # POST /user_sessions.json
   def create
     @user_session = UserSession.new(params[:user_session])
 
     respond_to do |format|
       if @user_session.save
-        format.html { redirect_to @user_session, notice: 'User session was successfully created.' }
+        format.html { redirect_to user_path(current_user), notice: 'Successfully logged in.' }
         format.json { render json: @user_session, status: :created, location: @user_session }
       else
         format.html { render action: "new" }
@@ -53,30 +22,11 @@ class UserSessionsController < ApplicationController
     end
   end
 
-  # PUT /user_sessions/1
-  # PUT /user_sessions/1.json
-  def update
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      if @user_session.update_attributes(params[:user_session])
-        format.html { redirect_to @user_session, notice: 'User session was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.json
   def destroy
-    @user_session = UserSession.find(params[:id])
-    @user_session.destroy
+    current_user_session.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_sessions_url }
+      format.html { redirect_to new_user_session_path }
       format.json { head :ok }
     end
   end
