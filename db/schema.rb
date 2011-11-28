1b966af312a17af741266fd3daa84d9214a8c272
B Kleinen <kleinen@infrastructure.de>
2011-11-28 18:24:42 +0100

    db migration for ideas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111121234557) do
+ActiveRecord::Schema.define(:version => 20111128164739) do
+
+  create_table "ideas", :force => true do |t|
+    t.string   "idea"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "user_sessions", :force => true do |t|
     t.datetime "created_at"
