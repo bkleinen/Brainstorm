b05a313cec371f1c6ab46a4c60e21c0aa59f7988
B Kleinen <kleinen@infrastructure.de>
2011-11-28 20:46:04 +0100

    updated defaults for brainstorm

--- app/models/brainstorm.rb
+++ app/models/brainstorm.rb
@@ -5,5 +5,8 @@ class Brainstorm < ActiveRecord::Base
      self.color  ||= '#ffffff'
      self.no_people ||= 6
      self.no_ideas ||= 3
+     self.date ||= Date.today.rfc3339
+     self.start ||= Time.now
+     self.end ||= self.start.advance(:hours => 1)
    end
 end
